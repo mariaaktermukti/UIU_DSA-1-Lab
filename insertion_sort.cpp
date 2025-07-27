e0bf24c41965c71a6a9d1adad0475c7c8a75a6d3
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-07-27 21:34:59 +0600

    Update insertion_sort.cpp

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -1,34 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 int main()
 {
     int n;
-    cout<< "Enter the number of elements of the array : ";
-    cin>> n;
+    cout << "Enter the size of an array: ";
+    cin >> n;
 
     int ar[n];
-
-    for(int i=0;i<n;i++)
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++)
     {
-        cin>> ar[i];
+        cin >> ar[i];
     }
 
-    for(int i=1;i<n;i++)
+    // Insertion Sort
+    for (int i = 1; i < n; i++)
     {
-        int key=ar[i];
-        int j= i-1;
-        while(j>=0 && ar[j]> key)
+        int key = ar[i];
+        int j = i - 1;
+        while (j >= 0 && ar[j] > key)
         {
-            ar[j+1]=ar[j];
+            ar[j + 1] = ar[j];
             j--;
         }
-        ar[j+1]=key;
+        ar[j + 1] = key;
     }
 
-    cout << "After insertion sort : ";
-    for (int i = 0; i < n; i++) 
+    cout << "After insertion sort: ";
+    for (int i = 0; i < n; i++)
     {
         cout << ar[i] << " ";
     }
+    cout << endl;
+
     return 0;
 }
