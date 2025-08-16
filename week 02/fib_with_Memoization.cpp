2879c8d6fc6389506e895ea4af0cab1993555041
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-08-16 16:45:32 +0600

    Add files via upload

---
+++ week 02/fib_with_Memoization.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long int dp[1005];  // complexity-> O(N)
+
+int fib(long long int n)
+{
+    if(n<2)
+    {
+        return n;
+    }
+    if(dp[n]!= -1)
+    {
+        return dp[n];
+    }
+    dp[n] = fib(n-1)+fib(n-2);
+    return dp[n];
+}
+
+int main()
+{
+    memset(dp,-1,sizeof(dp));
+    long long int n;
+    cin>> n;
+    cout << fib(n) << endl;
+    return 0;
+}
---
+++ week 02/fibo_using_recursion_DP.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fib(int n)
+{
+    if(n<2)
+    {
+        return n;
+    }
+    return fib(n-1)+fib(n-2);
+}
+
+int main()
+{
+    cout << fib(9) << endl;
+    return 0;
+}
