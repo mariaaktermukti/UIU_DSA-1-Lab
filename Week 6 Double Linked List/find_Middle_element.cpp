c42221e8b481898a0fccb02f5638b34ed461f18e
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-09-21 20:03:46 +0600

    Add files via upload

---
+++ Week 6 Double Linked List/find_Middle_element.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *prev;
+    Node *next;
+    Node(int val)
+    {
+        data = val;
+        prev = nullptr;
+        next = nullptr;
+    }
+};
+
+Node *head = nullptr;
+
+void insertAtEnd(int val)
+{
+    Node *newNode = new Node(val);
+    if (head == nullptr)
+    {
+        head = newNode;
+        return;
+    }
+    Node *temp = head;
+    while (temp->next != nullptr)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    newNode->prev = temp;
+}
+
+void findMiddle()
+{
+    if (head == nullptr)
+    {
+        cout << "List is empty!\n";
+        return;
+    }
+
+    Node *slow = head;
+    Node *fast = head;
+
+    while (fast != nullptr && fast->next != nullptr)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    cout << "Middle element is: " << slow->data << "\n";
+}
+
+void displayForward()
+{
+    Node *temp = head;
+    cout << "List: ";
+    while (temp != nullptr)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << "\n";
+}
+
+int main()
+{
+    int n, val;
+    cout << "How many nodes you want to insert? ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter value: ";
+        cin >> val;
+        insertAtEnd(val);
+    }
+
+    displayForward();
+    findMiddle();
+
+    return 0;
+}
